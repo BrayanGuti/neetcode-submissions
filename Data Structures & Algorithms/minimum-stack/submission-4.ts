3c82f3c1e5f7627f086beb7f1dd8937829f91117
Brayan Andres Gutierrez pupo <99362850+BrayanGuti@users.noreply.github.com>
2026-08-25 05:10:30 -0500

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.ts
@@ -0,0 +1,49 @@
+class MinStack {
+    constructor() {}
+
+    private stack = []
+    private minStack = []
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        const lastMin = this.minStack[this.minStack.length - 1]
+
+
+
+        if(val < (lastMin ?? Infinity)){
+            this.minStack.push(val)
+        } else {
+            this.minStack.push(lastMin)
+        }
+
+       
+        
+        this.stack.push(val)
+
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.minStack.pop()
+        this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1]
+    }
+}
