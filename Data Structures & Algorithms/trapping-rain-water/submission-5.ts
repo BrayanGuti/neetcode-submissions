83d50355810a056dd956bc7373d1b8c74f1724ba
Brayan Andres Gutierrez pupo <99362850+BrayanGuti@users.noreply.github.com>
2026-09-03 12:45:11 -0500

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height: number[]): number {
+
+
+        let left = 0
+        let right = height.length -1
+
+        let leftMax = height[left]
+        let rightMax = height[right]
+
+        let res = 0
+
+        while(right > left) {
+            if(leftMax < rightMax) {
+                left++
+                leftMax = Math.max(leftMax, height[left])
+                res += leftMax - height[left]
+            } else {
+                right--
+                rightMax = Math.max(rightMax, height[right])
+                res += rightMax - height[right]
+            }   
+        }
+
+        return res
+    }
+}
