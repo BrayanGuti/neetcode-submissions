cb53130526ea13b13f5972c74f83bb5b3519e0dd
Brayan Andres Gutierrez pupo <99362850+BrayanGuti@users.noreply.github.com>
2026-09-05 17:39:53 -0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.ts
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1: string, s2: string): boolean {
+        if(s1.length > s2.length) return false
+
+        const strMetadate = new Array(26).fill(0)
+        const indexs = new Set()
+
+        for(const char of s1) {
+            strMetadate[char.charCodeAt(0) - 'a'.charCodeAt(0)]++
+            indexs.add(char)
+        }
+
+        console.log(strMetadate)
+
+        let l = 0
+
+        for(let r = 0; r < s2.length; r++) {
+            if(indexs.has(s2[r])){
+                strMetadate[s2[r].charCodeAt(0) - 'a'.charCodeAt(0)]--
+            }
+        
+            while(r - l + 1 > s1.length) {
+                if(indexs.has(s2[l])){
+                    strMetadate[s2[l].charCodeAt(0) - 'a'.charCodeAt(0)]++
+                }
+                l++
+            }
+
+            if(strMetadate.every((a) => a === 0)){
+                console.log(l)
+                console.log(r)
+                return true
+            }
+        }
+
+        return false
+    }
+}
